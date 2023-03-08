23f9be05fa05ea06550c342a0a3e38f36b36de7f
Laurent Facente <facentelaurent@gmail.com>
2023-03-08 10:37:53 +0100

    Display book list + style materialize

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,26 @@ const App: FunctionComponent = () => {
  return (
     
   <div>
-    <h1>Mon étagère</h1>
-    <p>Il y a {books.length} livres sur mon etagère </p>
+    <h1 className='center'>Mon étagère</h1>
+    <div className='container'>
+        <div className='row'>
+            {books.map(({id , name, cover, author}) => (
+                <div className="col s6 m4" key={id}>
+                    <div className="card z-depth-5">
+                        <div className="card-image">
+                            <img src={cover} alt={name}/>
+                        </div>
+                        <div className="card-stacked">
+                        <div className="card-content">
+                            <h5>{name}</h5>
+                            <p>{author}</p>
+                        </div>   
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   </div>
 
  )
