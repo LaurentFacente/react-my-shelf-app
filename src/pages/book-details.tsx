85bd9fc0b8b36535ffe977e3abd60c25bccad565
Laurent Facente <facentelaurent@gmail.com>
2023-03-09 11:24:14 +0100

    creation page book details

---
+++ src/pages/book-details.tsx
@@ -0,0 +1,72 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
+import Book from '../models/book';
+import BOOKS from '../models/mock-books';
+import formatDate from '../tools/format-date';
+import formatType from '../tools/format-type';
+  
+type Params = { id: string };
+  
+const BooksDetail: FunctionComponent<RouteComponentProps<Params>> = ({ match }) => {
+    
+  const [book, setBook] = useState<Book|null>(null);
+  
+  useEffect(() => {
+    BOOKS.forEach(book => {
+      if (match.params.id === book.id.toString()) {
+        setBook(book);
+      }
+    })
+  }, [match.params.id]);
+    
+  return (
+    <div>
+      { book ? (
+        <div className="row">
+          <div className="col s12 m8 offset-m2"> 
+            <h2 className="header center">{ book.name }</h2>
+            <div className="card hoverable"> 
+              <div className="card-image">
+                <img src={book.cover} alt={book.name} style={{width: '250px', margin: '0 auto'}}/>
+              </div>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <table className="bordered striped">
+                  <tbody>
+                      <tr> 
+                        <td>Titre</td> 
+                        <td><strong>{ book.name }</strong></td> 
+                      </tr>
+                      <tr> 
+                        <td>Auteur</td> 
+                        <td><strong>{ book.author }</strong></td> 
+                      </tr> 
+                      <tr> 
+                        <td>Types</td> 
+                        <td>
+                          {book.types.map(type => (
+                           <span key={type} className={formatType(type)}>{type}</span>
+                          ))}</td> 
+                      </tr> 
+                      <tr> 
+                        <td>Date de création</td> 
+                        <td>{formatDate(book.created)}</td> 
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div className="card-action">
+                  <Link to="/">Retour</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h4 className="center">Aucun Livres à afficher !</h4>
+      )}
+    </div>
+  );
+}
+  
+export default BooksDetail;
