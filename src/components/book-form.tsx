45ecc10b426803040e4eb8cc4658b0e5d2ac961d
Laurent Facente <facentelaurent@gmail.com>
2023-03-13 00:17:59 +0100

    gestion etat formulaires edit

--- src/components/book-form.tsx
+++ src/components/book-form.tsx
@@ -1,17 +1,64 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Book from '../models/book';
 import formatType from '../tools/format-type';
   
 type Props = {
   book: Book
 };
+
+type Field = {
+    value?: any,
+    error?: string,
+    isValid?: boolean
+
+}
+
+type Form = {
+    name: Field,
+    author: Field,
+    types: Field
+}
   
 const BookForm: FunctionComponent<Props> = ({book}) => {
+
+    const [form, setForm] = useState<Form>({
+        name : {value: book.name, isValid: true},
+        author : {value: book.author, isValid: true},
+        types : {value: book.types, isValid: true} 
+    })
   
   const types: string[] = [
     'Roman', 'Productivité', 'Dev Perso', 'Socio', 'Science Fiction','Sport',
     'Historique', 'Psyco', 'Philosophique', 'Combat', 'Apprentissage'
   ];
+
+  const hasType = (type: string): boolean => {
+    return form.types.value.includes(type); 
+  }
+
+  const handleInputChange =(e: React.ChangeEvent<HTMLInputElement>) => {
+    const fieldName: string = e.target.name;
+    const fieldValue: string  = e.target.value;
+    const newField: Field = {[fieldName]: {value: fieldValue}};
+
+    setForm({...form, ...newField});
+  }
+
+  const selectType = (type: string, e: React.ChangeEvent<HTMLInputElement>): void => {
+    const checked = e.target.checked;
+    let newField: Field;
+
+    if(checked) {
+        const newTypes: string[] = form.types.value.concat([type]);
+        newField = { value : newTypes };
+    } else {
+        const newTypes: string[] = form.types.value.filter((currentType: string) => currentType !== type);
+        newField = { value : newTypes };
+    }
+
+    setForm({...form, ...{ types: newField }});
+  }
+
    
   return (
     <form>
@@ -26,12 +73,12 @@ const BookForm: FunctionComponent<Props> = ({book}) => {
                 {/* Book name */}
                 <div className="form-group">
                   <label htmlFor="name">Titre</label>
-                  <input id="name" type="text" className="form-control"></input>
+                  <input id="name" name="name" type="text" className="form-control" value={form.name.value} onChange={e => handleInputChange(e)}></input>
                 </div>
                 {/* Book author */}
                 <div className="form-group">
                   <label htmlFor="author">Auteur</label>
-                  <input id="author" type="text" className="form-control"></input>
+                  <input id="author" name="author" type="text" className="form-control" value={form.author.value} onChange={e => handleInputChange(e)}></input>
                 </div>
                 {/* Book types */}
                 <div className="form-group">
@@ -39,7 +86,7 @@ const BookForm: FunctionComponent<Props> = ({book}) => {
                   {types.map(type => (
                     <div key={type} style={{marginBottom: '10px'}}>
                       <label>
-                        <input id={type} type="checkbox" className="filled-in"></input>
+                        <input id={type} type="checkbox" className="filled-in" value={type} checked={hasType(type)} onChange={e => selectType(type, e)}></input>
                         <span>
                           <p className={formatType(type)}>{ type }</p>
                         </span>
